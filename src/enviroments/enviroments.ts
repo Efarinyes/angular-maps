ae321aefc82ad22d8ac7289f0fd1cf40849fb92f
Eduard Farinyes <efarinyes@gmail.com>
2023-10-20 17:52:47 +0200

    Update enviroments.ts

--- src/enviroments/enviroments.ts
+++ src/enviroments/enviroments.ts
@@ -2,5 +2,5 @@
 
 export const enviroment = {
   production: false,
-  apiKey: 'pk.eyJ1IjoiZWZhcmlueWVzIiwiYSI6ImNsODJ1d3NwdTAxNzUzdXFxbWl6a24zanUifQ.vSAnaqYDeUVgmVnxajeeZw'
+  apiKey: 'posa aqui la teva apiKey de Mapbox'
 }
